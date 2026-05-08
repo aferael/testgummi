520d2a07697d10b7665821b67ef2b11ba1468782
afe / aerin <57576856+aferael@users.noreply.github.com>
2026-05-08 15:09:51 -0700

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -33,3 +33,25 @@ document.addEventListener("DOMContentLoaded", async () => {
   charadex.tools.loadPage('.softload', 500);
   
 });
+
+
+
+/* ==================================================================== */
+/* Modal Gallery
+======================================================================= */
+function openModal(modalId, caption) {
+  let modal = document.getElementById(modalId);
+  modal.style.display = "flex";
+  modal.classList.add("show");
+  let message = modal.querySelector(".caption");
+  message.innerText = caption;
+}
+
+function closeModal(modalId) {
+  let modal = document.getElementById(modalId);
+  modal.classList.remove("show");
+  setTimeout(function () {
+    modal.style.display = "none";
+    modal.querySelector(".caption").innerText = "";
+  }, 300);
+};
