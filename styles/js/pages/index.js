46ee0df6712416b257cb20b4aeeccf29dd9fca94
afe / aerin <57576856+aferael@users.noreply.github.com>
2026-05-10 12:29:50 -0700

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -41,38 +41,19 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
 
-   // Designs
+  /* Designs
+  ===================================================================== */
   let designs = await charadex.initialize.page(null, charadex.page.index.designs, (arr) => {
-    let addDesigns = async () => {
-        if ($("#design-gallery").length != 0) {
-            if (charadex.page.index.designs.amount != 0) {
+    
+    // Splice the silly little array
+    let sliceAmount = charadex.page.index.designs.amount || 6;
+    arr.splice(sliceAmount, arr.length);
+    
+    // Filter out any MYO slots, reverse and pull the first 4
+    let selectDesigns = designs.filter((i) => { return i.designtype != 'MYO Slot' }).reverse().slice(0, charadex.page.index.designs.amount);
 
-                // Grab dah sheet
-                let designs = await charadex.initialize.page(null, charadex.page.index.masterlist);
-
-                // Filter out any MYO slots, reverse and pull the first 4
-                let selectDesigns = designs.filter((i) => { return i.designtype != 'MYO Slot' }).reverse().slice(0, charadex.page.index.designs.amount);
-
-                // Add cardlink
-                let cardKey = Object.keys(selectDesigns[0])[0];
-                for (var i in selectDesigns) { selectDesigns[i].cardlink = folderURL + "masterlist.html?" + cardKey + "=" + selectDesigns[i][cardKey]; }
-
-                // Nyoom
-                let galleryOptions = {
-                    item: 'design-item',
-                    valueNames: sheetArrayKeys(selectDesigns),
-                };
-
-                // Render Gallery
-                let charadex = new List('design-gallery', galleryOptions, selectDesigns);
-
-            } else {
-                $("#design-gallery").hide();
-            }
-        }
-    }; addDesigns();
   });
-
+                
   /* Load Page
   ===================================================================== */
   charadex.tools.loadPage('.softload', 500);
