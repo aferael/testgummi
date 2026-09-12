7ee371abd1689014a7e56edc5474d54ca425429e
afe / aerin <57576856+aferael@users.noreply.github.com>
2026-09-11 17:05:33 -0700

    Create badgecollection.js

---
+++ styles/js/pages/badgecollection.js
@@ -0,0 +1,36 @@
+/* ==================================================================== */
+/* Import Charadex
+======================================================================= */
+import { charadex } from '../charadex.js';
+
+
+/* ==================================================================== */
+/* Load
+======================================================================= */
+document.addEventListener("DOMContentLoaded", async () => {
+
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.inventory,
+    null, 
+    async (listData) => {
+
+      if (listData.type == 'profile') {
+
+        let profile = listData.profileArray[0];
+
+        // Inventory
+        charadex.initialize.groupGallery(
+          charadex.page.badgeCollection.badgeCollectionConfig,
+          await charadex.manageData.badgeCollectionFix(profile),
+          'type',
+          charadex.url.getPageUrl('badges')
+        )
+        
+      }
+    }
+  );
+  
+  charadex.tools.loadPage('.softload', 500);
+  
+});
