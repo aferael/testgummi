a714e4be1b5517ef66fb42739b605fff9765e84f
afe / aerin <57576856+aferael@users.noreply.github.com>
2026-09-13 14:15:26 -0700

    Update collection.js

--- styles/js/pages/collection.js
+++ styles/js/pages/collection.js
@@ -27,27 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
           charadex.url.getPageUrl('badges')
         )
 
-        // Designs
-        if (charadex.tools.checkArray(profile.masterlist)) {
-          let designs = await charadex.initialize.page(
-            profile.masterlist,
-            charadex.page.inventory.relatedData['masterlist'],
-          );
-        }
-
-        // Logs
-        if (charadex.tools.checkArray(profile.inventorylog)) {
-          let logs = await charadex.initialize.page(
-            profile.inventorylog,
-            charadex.page.inventory.relatedData['inventory log'],
-          );
-        }
-
-
       }
     }
   );
-  
-  charadex.tools.loadPage('.softload', 500);
-  
 });
